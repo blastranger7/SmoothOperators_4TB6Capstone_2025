7c9acd85a766fd5745c549310f96d16a74978435
derbalaa <derbalaa@mcmaster.ca>
2024-10-29 16:11:20 -0400

    Added state machine.

--- ProofOfConceptDemo/PrototypeDemo.cpp
+++ ProofOfConceptDemo/PrototypeDemo.cpp
@@ -15,6 +15,15 @@ int potVal = 0;
 int direction[] = {1, -1};
 int dindex = 0;
 
+
+int state = 1;
+int prevState = 0;
+bool StateInit = state != prevState;
+
+bool cmdGrip = true;
+int numSteps;
+int timeCounter = 0;
+
 void setup() {
   // Set the serial monitor baudrate to 9600
   Serial.begin(9600);
@@ -31,23 +40,70 @@ void loop() {
   // analogRead function returns the integer 10 bit integer (0 to 1023)
   level0 = analogRead(0);
   level1 = analogRead(1);
-  
-  // Print "Analog value:" in serial monitor
-  Serial.print("Analog value: ");
-  Serial.print(level0);
-  Serial.print("\tAnalog value: ");
-  Serial.println(level1);
-  
-  if(level0 > 50 || level1 >50){
-    dindex = 1;
+
+  switch(state){
+    1: // Wait for grip command
+      if(StateInit) {
+        numSteps = 0;
+        break;
+      }
+      if(cmdGrip) {
+        state = 2;
+        numSteps = 15;
+        dindex = 1;
+        cmdGrip = false; //temp for code test
+        break;
+      }
+
+    2: // Check sensors to ensure object is gripped
+      if(level0 > 50 || level1 >50){
+        // Print "Analog value:" in serial monitor
+        Serial.print("Analog value: ");
+        Serial.print(level0);
+        Serial.print("\tAnalog value: ");
+        Serial.println(level1);
+        numSteps = 0;
+        break;
+      }
+
+    3: // Wait a few seconds
+      if (StateInit) {
+        timeCounter = 0;
+        break;
+      }
+      timeCounter += 1;
+      if timeCounter >= 1000 {
+        state = 4;
+        break;
+      }
+
+    4: // Release object state
+      numSteps = 15;
+      dindex = -1;
+      timeCounter += 1;
+      if timeCounter >= 1000 {
+        state = 5;
+        break;
+      }
+
+    5:
+      if (StateInit) {
+        timeCounter = 0;
+        break;
+      }
+      timeCounter += 1;
+      if timeCounter >= 1000 {
+        state = 1;
+        cmdGrip = true;
+        break;
+      }
+
   }
-  else if (level0 <= 50 && level1 <=50) {
-    dindex = 0;
-  }
+  
+  StateInit = state != prevState;
+  prevState = state;
 
-  stepper.step(direction[dindex]*15);
-
-
+  stepper.step(direction[dindex]*numSteps);
   
   // Turn off all the led initially
 
