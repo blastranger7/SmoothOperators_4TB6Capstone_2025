6062fc87a898ac898d46c2d66edc050c9e4799c7
Smit Patel <Smitypatel03@yahoo.com>
2025-01-18 19:43:22 -0500

    fix servo_motor files

--- RobotCode/Modules/servo_motor.c
+++ RobotCode/Modules/servo_motor.c
@@ -1,6 +1,6 @@
 #include "servo_motor.h"
 
-last_motor_req[4] = {};
+int last_motor_req[4] = {};
 
 //returns the last angle requested for a given motor 
 int get_motor_position(int motor_num) {
@@ -13,28 +13,30 @@ int get_motor_position(int motor_num) {
             return last_motor_req[2];
         case 3:
             return last_motor_req[3];
+				default:
+					return 0;
     }
 }
 
 //sets a motor to a specific angle
-void set_motor_position(int motor_num, int angle) {
-    int pulse = 750 + (ccr_per_degree * angle); //convert the requested angle into a ccr value
+void set_motor_position(TIM_HandleTypeDef htim, int channel_num, int angle) {
+    int pulse = 750 + (1 * angle); //convert the requested angle into a ccr value
     
-    switch(motor_num) {
-        case 0: //base
-            __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pulse);
+    switch(channel_num) {
+        case 1: //base
+            __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_1, pulse);
             last_motor_req[0] = angle;
 
-        case 1: //joint 1
-            __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, pulse);
+        case 2: //joint 1
+            __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_2, pulse);
             last_motor_req[1] = angle;
 
-        case 2: //joint 2
-            __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, pulse);
+        case 3: //joint 2
+            __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_3, pulse);
             last_motor_req[2] = angle;
 
-        case 3: //joint 3
-            __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, pulse);
+        case 4: //joint 3
+            __HAL_TIM_SET_COMPARE(&htim, TIM_CHANNEL_4, pulse);
             last_motor_req[3] = angle;
     }
 }
--- RobotCode/Modules/servo_motor.h
+++ RobotCode/Modules/servo_motor.h
@@ -1,13 +1,14 @@
 #ifndef SERVO_MOTOR_H
 #define SERVO_MOTOR_H
 
-#include "stm32fxx_hal_tim.h"
+#include "main.h"
+#include "stm32f4xx_hal_tim.h"
 
 #define ccr_per_degree 5.55
 
 extern int last_motor_req[4];
 
 int get_motor_position(int motor_num);
-int set_motor_position(int motor_num, int angle);
+void set_motor_position(TIM_HandleTypeDef htim, int channel_num, int angle);
 
 #endif
