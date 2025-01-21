7bbc5ffa972ffeede4a4111104594fbb67598723
Smit Patel <Smitypatel03@yahoo.com>
2025-01-20 23:50:22 -0500

    UART_PI module

---
+++ RobotCode/Modules/uart_pi.c
@@ -0,0 +1,25 @@
+#include "uart_pi.h"
+
+uart_port initUART(UART_HandleTypeDef* port) {
+    uart_port uart;
+    
+    uart.port = port;
+    uart.rx_buffer = {};
+    uart.updated = 0;
+
+    return uart;
+}
+
+void sendData(uart_port* uart, uint8_t data) {
+    HAL_UART_Transmit_IT(&uart, &data, 1);
+}
+
+void getData(uart_port* uart, uint8_t* data) {
+    for (int i = 0; i < 2; i++) {
+        *(data + i) = uart->rx_buffer[i];
+    }
+}
+
+int isDataUpdated(uart_port* uart) {
+    return uart->updated; 
+}
---
+++ RobotCode/Modules/uart_pi.h
@@ -0,0 +1,17 @@
+#ifndef UART_PI_H
+#define UART_PI_H
+
+#include "stm32f4xx_hal_uart.h"
+
+typedef struct {
+    UART_HandleTypeDef* port;
+    uint8_t rx_buffer[3];
+    int updated;
+} uart_port; 
+
+uart_port initUART(UART_HandleTypeDef* port);
+void sendData(uart_port* uart, uint8_t data);
+void getData(uart_port* uart, uint8_t* data);
+int isDataUpdated(uart_port* uart);
+
+#endif
