40eac0be1af6334dd976446fea9fe33adfe7c364
Smit Patel <Smitypatel03@yahoo.com>
2025-02-22 16:01:20 -0500

    GRIPPER_CONTROL module

--- RobotCode/Modules/gripper_control.c
+++ RobotCode/Modules/gripper_control.c
@@ -1,13 +1,64 @@
 #include "gripper_control.h"
 
-r_gripper* init_Gripper(r_gripper* gripper, TIM_HandleTypeDef* timer, uint32_t channel, GPIO_HandleTypeDef* l_GPIO, uint16_t l_pin, GPIO_HandleTypeDef* r_GPIO, uint16_t r_pin) {
-    gripper->
+//dummy values
+#define grip_threshold 100;
+#define hysteresis_band 50;
+
+static int current_position;
+static double current_force;
+static int gripper_step_size = 1;
+
+void init_Gripper(r_gripper* gripper, TIM_HandleTypeDef* timer, uint32_t channel, GPIO_HandleTypeDef* l_GPIO, uint16_t l_pin, GPIO_HandleTypeDef* r_GPIO, uint16_t r_pin) {
+    //init gripper motor
+    gripper->motor->timer = timer;
+    gripper->motor->channel = channel;
+    gripper->motor->current_angle = 0;
+
+    //init force sensors
+    gripper->l_sensor->GPIOx = l_GPIO;
+    gripper->l_sensor->GPIO_Pin = l_pin;
+    gripper->l_sensor->reading = 0;
+
+    gripper->r_sensor->GPIOx = r_GPIO;
+    gripper->r_sensor->GPIO_Pin = r_pin;
+    gripper->r_sensor->reading = 0;
 }
 
 void openGripper(r_gripper* gripper) {
+    updatePosition(gripper);
+    updateForce(gripper);
 
+    if (current_force < grip_threshold) { continue; }
+    else { //open the gripper until the force is below the threshold
+        while (current_force >= grip_threshold) {
+            setMotorPosition(gripper->motor, current_position - gripper_step_size);
+            updatePosition();
+            updateForce();
+        }
+    }
 }
 
 void closeGripper(r_gripper* gripper) {
+    updatePosition(gripper);
+    updateForce(gripper);
 
+    if (current_force >= (grip_threshold + hysteresis_band)) { continue; }
+    else { //close the gripper until the force threshold is reached
+        while (current_force < (grip_threshold + hysteresis_band)) {
+            setMotorPosition(gripper->motor, current_position + gripper_step_size);
+            updatePosition();
+            updateForce();
+        }
+    }
+}
+
+static void updatePosition(r_gripper* gripper) {
+    current_position = gripper->motor->current_angle;
+}
+
+static void updateForce(r_gripper* gripper) {
+    double left_reading = gripper->l_sensor->reading;
+    double right_reading = gripper->r_sensor->reading;
+
+    current_force = (left_reading > right_reading) ? left_reading : right_reading;
 }
--- RobotCode/Modules/gripper_control.h
+++ RobotCode/Modules/gripper_control.h
@@ -10,7 +10,7 @@ typedef struct {
     f_sensor* r_sensor;
 } r_gripper;
 
-r_gripper* init_Gripper(r_gripper* gripper, TIM_HandleTypeDef* timer, uint32_t channel, GPIO_HandleTypeDef* l_GPIO, uint16_t l_pin, GPIO_HandleTypeDef* r_GPIO, uint16_t r_pin);
+void init_Gripper(r_gripper* gripper, TIM_HandleTypeDef* timer, uint32_t channel, GPIO_HandleTypeDef* l_GPIO, uint16_t l_pin, GPIO_HandleTypeDef* r_GPIO, uint16_t r_pin);
 void openGripper(r_gripper* gripper);
 void closeGripper(r_gripper* gripper);
 
